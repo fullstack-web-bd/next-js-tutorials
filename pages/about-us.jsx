55dd2f1df3f8536756f09b77e51e716d6b05a83d
maniruzzaman.akash <maniruzzaman.akash@paysera.net>
2023-04-11 03:39:44 +0600

    Next JS first Day 01

---
+++ pages/about-us.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <>
+            <h2>About us</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure ex voluptate commodi tempore, quo ullam quas sequi debitis sapiente a accusamus explicabo nulla repudiandae. Harum fugiat doloribus distinctio excepturi officiis!</p>
+        </>
+    )
+}
---
+++ pages/profile/me.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+        <>
+            <h2>Profile</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure ex voluptate commodi tempore, quo ullam quas sequi debitis sapiente a accusamus explicabo nulla repudiandae. Harum fugiat doloribus distinctio excepturi officiis!</p>
+        </>
+    )
+}
